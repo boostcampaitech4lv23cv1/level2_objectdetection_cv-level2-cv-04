d8385fd3ee5fa77fe3a81c1777fc75870a23deb9
brotherhoon <brotherhoon88@gmail.com>
2022-11-23 18:03:18 +0000

    !HOTFIX: modify wandblogger_hook.py

--- baseline/mmdetection/configs/_khh_custom_configs/config4_baseline_dy.py
+++
@@ -1,6 +0,0 @@
-_base_ = [
-    './_base_/models/faster_rcnn_x101_64x4d_fpn_1x_coco.py',
-    './_base_/datasets/trash_detection.py',
-    './_base_/schedules/schedule_1x.py',
-    './_base_/runtimes/default_runtime.py'
-]
