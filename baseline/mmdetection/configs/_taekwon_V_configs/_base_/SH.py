80a77246dd37b2e46cbe956a2404f247094d5864
sangheon <qwer55252@gmail.com>
2022-11-27 17:24:56 +0000

    Feat: SH.py update

--- baseline/mmdetection/configs/_taekwon_V_configs/_base_/SH.py
+++ baseline/mmdetection/configs/_taekwon_V_configs/_base_/SH.py
@@ -1,21 +1,29 @@
 model = dict(
     type='CascadeRCNN',
     backbone=dict(
-        type='ResNeXt',
-        depth=101,
-        num_stages=4,
+        type='SwinTransformer',
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        patch_norm=True,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch',
+        with_cp=False,
+        convert_weights=True,
         init_cfg=dict(
-            type='Pretrained', checkpoint='open-mmlab://resnext101_32x4d'),
-        groups=32,
-        base_width=4),
+            type='Pretrained',
+            checkpoint=
+            'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window7_224_22k.pth'
+        )),
     neck=dict(
         type='FPN',
-        in_channels=[256, 512, 1024, 2048],
+        in_channels=[192, 384, 768, 1536],
         out_channels=256,
         num_outs=5),
     rpn_head=dict(
